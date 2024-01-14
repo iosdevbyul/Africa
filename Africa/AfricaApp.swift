e21d1d4577460d0283fd99dd8c968791d2f72dab
Comatoki <hbabnd@gmail.com>
2024-01-14 20:38:47 +0900

    Initial Commit

---
+++ Africa/AfricaApp.swift
@@ -0,0 +1,17 @@
+//
+//  AfricaApp.swift
+//  Africa
+//
+//  Created by COMATOKI on 2024-01-14.
+//
+
+import SwiftUI
+
+@main
+struct AfricaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
