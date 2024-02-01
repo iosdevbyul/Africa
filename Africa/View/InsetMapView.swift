d52124c936c2532f2c487649f5107351634ff011
comatoki <hbabnd@gmail.com>
2024-02-01 19:21:01 +0900

    Feat - add more UIs on detail view

---
+++ Africa/View/InsetMapView.swift
@@ -0,0 +1,18 @@
+//
+//  InsetMapView.swift
+//  Africa
+//
+//  Created by COMATOKI on 2024-02-01.
+//
+
+import SwiftUI
+
+struct InsetMapView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    InsetMapView()
+}
