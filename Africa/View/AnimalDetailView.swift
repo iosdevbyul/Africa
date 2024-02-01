8b605f296f60f6873e3d3cbfc4cab1aefbb93bfe
comatoki <hbabnd@gmail.com>
2024-02-01 18:07:30 +0900

    Feat - Add animalDetailView

---
+++ Africa/View/AnimalDetailView.swift
@@ -0,0 +1,18 @@
+//
+//  AnimalDetailView.swift
+//  Africa
+//
+//  Created by COMATOKI on 2024-02-01.
+//
+
+import SwiftUI
+
+struct AnimalDetailView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    AnimalDetailView()
+}
---
+++ Africa/View/HeadingView.swift
@@ -0,0 +1,18 @@
+//
+//  HeadingView.swift
+//  Africa
+//
+//  Created by COMATOKI on 2024-02-01.
+//
+
+import SwiftUI
+
+struct HeadingView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    HeadingView()
+}
