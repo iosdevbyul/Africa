b585506d5b4f701cfb57b7290632b25a79322215
comatoki <hbabnd@gmail.com>
2024-04-20 17:03:31 +0900

    Feat: Map added

--- Africa/App/MapView.swift
+++ Africa/App/MapView.swift
@@ -6,13 +6,39 @@
 //
 
 import SwiftUI
+import MapKit
 
 struct MapView: View {
+    
+    @State private var region: MKCoordinateRegion = {
+        var mapCoordinates = CLLocationCoordinate2D(latitude: 6.600286, longitude: 16.4377599)
+        var mapZoomLevel = MKCoordinateSpan(latitudeDelta: 70.0, longitudeDelta: 70.0)
+        var mapRegion = MKCoordinateRegion(center: mapCoordinates, span: mapZoomLevel)
+        return mapRegion
+    }()
+    
+    let locations: [NationalParkLocation] = Bundle.main.decode("locations.json")
+    
     var body: some View {
-        Text("Map")
+        // Mark:- 1 Basic Map
+        Map(coordinateRegion: $region, annotationItems: locations) { item in
+            //option 1
+//            MapPin(coordinate: item.location, tint: .accentColor)
+            //option 2
+//            MapMarker(coordinate: item.location, tint: .accentColor)
+            //option 3
+            MapAnnotation(coordinate: item.location) {
+                Image("logo")
+                    .resizable()
+                    .scaledToFit()
+                    .frame(width: 32, height: 32, alignment: .center)
+            }
+        }
+        
+        
     }
 }
 
-#Preview {
-    MapView()
-}
+//#Preview {
+//    MapView()
+//}
