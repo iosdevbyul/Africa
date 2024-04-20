70b0dc0442732795821f283ca012a4018f080556
comatoki <hbabnd@gmail.com>
2024-04-20 20:19:12 +0900

    Feat : Gallery bg animation added

---
+++ Africa/View/MotionAnimationView.swift
@@ -0,0 +1,18 @@
+//
+//  MotionAnimationView.swift
+//  Africa
+//
+//  Created by COMATOKI on 2024-04-20.
+//
+
+import SwiftUI
+
+struct MotionAnimationView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    MotionAnimationView()
+}
